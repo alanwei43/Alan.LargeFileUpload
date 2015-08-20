95f5a3446e1a9de78f6a03b4474425c0a1b1902e
Alan <alan.wei@live.com>
2015-08-20 19:45:57 +0800

    initialize

---
+++ Alan.LargeFileUpload/FileUpload.ashx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alan.LargeFileUpload
+{
+    /// <summary>
+    /// Summary description for FileUpload
+    /// </summary>
+    public class FileUpload : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var req = context.Request;
+            var rep = context.Response;
+
+            req.ContentType = "application/json";
+            var fileName = req["fileName"];
+
+            if (String.IsNullOrWhiteSpace(fileName))
+            {
+                fileName = Guid.NewGuid().ToString();
+            }
+            using (System.IO.BinaryReader reader = new System.IO.BinaryReader(req.InputStream))
+            {
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                int bte;
+                while ((bte = reader.Read()) != -1)
+                {
+                    ms.WriteByte((byte)bte);
+                }
+                var arrayBytes = ms.ToArray();
+                var base64 = System.Text.Encoding.UTF8.GetString(arrayBytes);
+                var data = Convert.FromBase64String(base64);
+
+                var fileFullPath = System.Web.Hosting.HostingEnvironment.MapPath("~/Static/" + fileName);
+
+                this.AppedOrCreate(fileFullPath, data);
+            }
+            rep.Write("{\"FileName\":\"" + fileName + "\"}");
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// 如果文件存在就附加数据, 如果不存在就新建文件
+        /// </summary>
+        /// <param name="filePath">文件绝对路径</param>
+        /// <param name="data">数据</param>
+        private void AppedOrCreate(string filePath, byte[] data)
+        {
+            if (!System.IO.File.Exists(filePath))
+            {
+                var fs = System.IO.File.Create(filePath);
+                fs.Close();
+                fs.Dispose();
+            }
+
+            using (System.IO.FileStream fs = new System.IO.FileStream(filePath, System.IO.FileMode.Append, System.IO.FileAccess.Write, System.IO.FileShare.Write))
+            {
+                fs.Write(data, 0, data.Length);
+            }
+        }
+
+    }
+}
---
+++ Alan.LargeFileUpload/SeperateFile.js
@@ -0,0 +1,163 @@
+﻿/// <reference path="E:\Projects\Alan.LargeFileUpload\Alan.LargeFileUpload\jquery-1.11.2.js" />
+
+/*
+ * Author: Alan Wei
+ * Email: Alan.Wei@live.com
+ * Github: https://github.com/Allen-Wei/Alan.LargeFileUpload
+ * 
+ * Description: 使用Javascript分割文件, 实现分块异步的断点续传上传功能.
+ */
+
+(function () {
+
+
+    /*
+     * 
+     * @param file: File对象
+     * @param userOptions:{
+     *      start: 文件上传开始, 
+     *      send: 发送数据, 
+     *      sending: 正在发送数据, 
+     *      sent: 数据发送完成, 
+     *      finish: 文件上传结束, 
+     *      error: 文件上传出错,
+     *      blockSize: 每次上传的数据量大小(单位byte)
+     * }
+     * @returns {
+     *      continueSeperate: 断点续传
+     * } 
+     */
+    window.SeperateFile = function (file, userOptions) {
+        if (!file || !file.name || !file.size || !file.slice) {
+            throw "invalid file";
+        }
+
+        var reader = new FileReader();
+
+        var options = {
+            start: function () {
+                $("body").trigger("fuStart.fileupload");
+            },
+            send: function (queue, queues) {
+                $("body").trigger("fuSend.fileupload", [queue, queues]);
+                var deferred = $.Deferred();
+                deferred.resolve();
+                return deferred.promise();
+            },
+            sending: function (queue, queues) {
+                $("body").trigger("fuSending.fileupload", [queue, queues]);
+            },
+            sent: function (queue, queues) {
+                $("body").trigger("fuSent.fileupload", [queue, queues]);
+            },
+            finish: function (queues) {
+                $("body").trigger("fuFinish.fileupload", [queues]);
+            },
+            error: function () { }
+        };
+
+        var uploadConfig = {
+            totalSize: file.size,
+            blockSize: 100 * 1024,
+            timeOut: 100,
+            queues: [
+                {
+                    start: 0,
+                    end: NaN,
+                    status: 'unsend',
+                    data: undefined
+                }
+            ]
+        };
+
+        if (userOptions) {
+            $.extend(options, userOptions);
+            uploadConfig.blockSize = userOptions.blockSize || uploadConfig.blockSize;
+        }
+
+
+        if (uploadConfig.totalSize <= 0) {
+            throw "Uplaod file size <= 0";
+        }
+
+        (function () {
+            //initial first queue
+            var firstQueue = {
+                start: 0,
+                end: NaN,
+                status: 'unsend',
+                data: undefined
+            };
+            firstQueue.end = firstQueue.start + uploadConfig.blockSize;
+            firstQueue.end = firstQueue.end > uploadConfig.totalSize ? uploadConfig.totalSize : firstQueue.end;
+            uploadConfig.queues[0] = firstQueue;
+        })();
+
+
+        var interval = function () {
+
+            var lastQueue = uploadConfig.queues[uploadConfig.queues.length - 1]; //last queue
+
+            if (lastQueue.end > uploadConfig.totalSize) {
+                options.finish(uploadConfig.queues);
+                return;
+            }
+            if (lastQueue.status === 'unsend') {
+                //lastQueue unsend
+                lastQueue.status = 'sending'; //change last queue status to sending
+                var block = file.slice(lastQueue.start, lastQueue.end);
+                reader.readAsDataURL(block);
+                reader.onload = function () {
+                    var base64 = reader.result.split(',')[1];
+                    lastQueue.data = base64;
+                    var promise = options.send(lastQueue, uploadConfig.queues);
+                    promise.then(function () {
+                        lastQueue.status = 'sent';
+                        setTimeout(interval, uploadConfig.timeOut);
+                    }, function () {
+                        lastQueue.status = 'unsend';
+                        options.error(lastQueue);
+                    });
+                };
+            }
+            if (lastQueue.status === 'sending') {
+                //lastQueue正在发送
+                options.sending(lastQueue, uploadConfig.queues);
+            }
+            if (lastQueue.status === 'sent') {
+                //lastQueue已经发送
+                if (lastQueue.end >= uploadConfig.totalSize) {
+                    options.finish(uploadConfig.queues);
+                    return;
+                }
+
+                var endSize = lastQueue.end + uploadConfig.blockSize;
+                endSize = endSize > uploadConfig.totalSize ? uploadConfig.totalSize : endSize;
+
+                uploadConfig.queues.push({
+                    start: lastQueue.end,
+                    end: endSize,
+                    status: 'unsend'
+                });
+                options.sent(lastQueue, uploadConfig.queues);
+                setTimeout(interval, uploadConfig.timeOut);
+            }
+        };
+
+        options.start();
+        setTimeout(interval, uploadConfig.timeOut);
+
+        var utils = {
+            //Continue upload
+            continueSeperate: function () {
+                setTimeout(interval, uploadConfig.timeOut);
+            },
+            getQueues: function () {
+                return uploadConfig.queues;
+            }
+        };
+        return utils;
+    };
+
+
+})();
